6ce55c2001842fe016573b922abe014122f78f55
Luis Alvarenga <luisfam@hotmail.com>
2024-03-09 21:08:22 -0600

    Changed namespace to IronPython2

---
+++ Src/IronPython2.Modules/_sysconfigdata.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using IronPython2.Runtime;
+
+[assembly: PythonModule("_sysconfigdata", typeof(IronPython2.Modules._sysconfigdata), PlatformsAttribute.PlatformFamily.Unix)]
+namespace IronPython2.Modules {
+    public static class _sysconfigdata
+    {
+        public static PythonDictionary build_time_vars = new PythonDictionary();
+    }
+}
